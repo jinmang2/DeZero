8a6621d21c46dd0db67b5d8e7b71eea0d70867c7
jinmang2 <jinmang2@gmail.com>
2021-03-15 11:13:10 +0900

    [제 1고지] Step01 상자로서의 변수 - 상자와 데이터는 별개 - 상자에는 데이터가 들어간다(대입 혹은 할당한다) - 상자 속을 들여다보면 데이터를 알 수 있다(참조한다)

---
+++ steps/highland1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+if __name__ == '__main__':
+    data = np.array(1.0)
+    x = Variable(data)
+    print(x.data)
+
+    x.data = np.array(2.0)
+    print(x.data)
