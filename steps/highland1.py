6894ec65d7d3ed645fe718fbea2ac52f7b3d2858
jinmang2 <jinmang2@gmail.com>
2021-03-15 11:28:49 +0900

    [제 1고지] Step04 수치 미분 - 미분근사함수 func:numerical_diff를 구현 (중앙차분) - 중앙차분이 전진차분보다 오차가 적은 이유는 Taylor Series를 이용해 증명 가능 (http://ohiouniversityfaculty.com/youngt/IntNumMeth/lecture27.pdf) - 미분을 해석적으로 분석하는 것은 수식으로 답을 계산한다는 의미 - 합성함수도 자동으로 계산 가능함 - 수치 미분은 다양한 문제를 가지고 있음   1. 자릿수 누락으로 계산이 정확하지 않음   2. 계산량이 많음 + 다변량의 경우 변수를 각각 미분해야함 - 수치 미분의 장점? 구현하기 쉽고 거의 정확한 값을 얻을 수 있음 (gradient checking)

--- steps/highland1.py
+++ steps/highland1.py
@@ -30,13 +30,28 @@ class Exp(Function):
         return np.exp(x)
 
 
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
 if __name__ == '__main__':
-    A = Square()
-    B = Exp()
-    C = Square()
+    f = Square()
+    x = Variable(np.array(2.0))
+    dy = numerical_diff(f, x)
+    print(dy) # 4.000000000004
+
+
+    def f(x):
+        A = Square()
+        B = Exp()
+        C = Square()
+        return C(B(A(x)))
+
 
     x = Variable(np.array(0.5))
-    a = A(x)
-    b = B(a)
-    y = C(b)
-    print(y.data) # 1.648721270700128
+    dy = numerical_diff(f, x)
+    print(dy) # 3.2974426293330694
