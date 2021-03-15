83a5ebb3d8a0c2775b2dc68160c8a27741295a29
jinmang2 <jinmang2@gmail.com>
2021-03-15 14:03:30 +0900

    Copy highland1.py to highland2.py - Variable, Function만 남기고 나머지 다 제거

---
+++ steps/highland2.py
@@ -0,0 +1,55 @@
+import numpy as np
+from overrides import overrides
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None and not isinstance(data, np.ndarray):
+            raise TypeError('{} is not supported'.format(type(data)))
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self)
+        self.input = input
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    pass
