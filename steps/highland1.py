1d0cacf3c9ac6fb905cc6ab4cf244b32e8411c89
jinmang2 <jinmang2@gmail.com>
2021-03-15 13:23:33 +0900

    [제 1고지] Step10 테스트 - unittest 진행! - 보통 tests파일에 모아두는 경우가 많음 - 직접 testcase를 작성, 실험해보는 방식 - 그리고 SW개발할 때 CI(continuous integration) 서비스와 연계하여 매시간 테스트가 실행되도록 설정할 수 있음

--- steps/highland1.py
+++ steps/highland1.py
@@ -1,3 +1,4 @@
+import unittest
 import numpy as np
 from overrides import overrides
 
@@ -89,6 +90,29 @@ def numerical_diff(f, x, eps=1e-4):
     y0 = f(x0)
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
+
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x = Variable(np.random.randn(1))
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
 
 
 if __name__ == '__main__':
