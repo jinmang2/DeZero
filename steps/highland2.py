98b204f2c920cfaf913f6f9a226488e3d7828ec2
jinmang2 <jinmang2@gmail.com>
2021-03-19 15:33:15 +0900

    [제 2고지] Step24 복잡한 함수의 미분 - 최적화 문제에서 자주 사용되는 테스트 함수로 DeZero의 능력을 확인해보자! - Test functions for optimization(https://en.wikipedia.org/wiki/Test_functions_for_optimization)

--- steps/highland2.py
+++ steps/highland2.py
@@ -5,8 +5,6 @@ from overrides import overrides
 
 
 # ==============================================================
-""" Package로 정리! """
-# Add import path for the dezero directory.
 if '__file__' in globals():
     import os
     import sys
@@ -16,12 +14,27 @@ import numpy as np
 from dezero import Variable
 
 
-x = Variable(np.array(1.0))
-y = (x + 3) ** 2
-y.backward()
+def sphere(x, y):
+    z = x ** 2 + y ** 2
+    return z
 
-print(y) # variable(16.0)
-print(x.grad) # 8.0
+
+def matyas(x, y):
+    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+    return z
+
+
+def goldstein(x, y):
+    z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
+        (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+    return z
+
+
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = goldstein(x, y)  # sphere(x, y) / matyas(x, y)
+z.backward()
+print(x.grad, y.grad) # -5376.0, 8064.0
 # ==============================================================
 
 
