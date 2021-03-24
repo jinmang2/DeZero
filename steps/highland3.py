f366e3b215f46b9ba5fb8b06b5a71f8c9931f311
jinmang2 <jinmang2@gmail.com>
2021-03-24 10:01:35 +0900

    [제 3고지] Step27 테일러 급수 미분 - numpy의 sin과 Taylor 급수로 근사한 값을 비교 - threshold를 작게 할수록 이론상으로 근사 정밀도가 좋아지지만 자릿수 누락, 반올림 등 컴퓨터 계산이 발생할 수 있음

--- steps/highland3.py
+++ steps/highland3.py
@@ -1,26 +1,52 @@
-'''
-Need the dot binary from the graphviz package (www.graphviz.org).
-'''
 if '__file__' in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import math
 import numpy as np
-from dezero import Variable
+from dezero import Variable, Function
 from dezero.utils import plot_dot_graph
 
 
-def goldstein(x, y):
-    z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
-        (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
-    return z
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        return gy * np.cos(x)
 
 
-x = Variable(np.array(1.0))
-y = Variable(np.array(1.0))
-z = goldstein(x, y)
-z.backward()
+def sin(x):
+    return Sin()(x)
+
+
+x = Variable(np.array(np.pi / 4))
+y = sin(x)
+y.backward()
+print('--- original sin ---')
+print(y.data) # 0.7071067811865476
+print(x.grad) # 0.7071067811865476
+
+
+def my_sin(x, threshold=0.0001):
+    y = 0
+    for i in range(100000):
+        c = (-1) ** i / math.factorial(2 * i + 1)
+        t = c * x ** (2 * i + 1)
+        y = y + t
+        if abs(t.data) < threshold:
+            break
+    return y
+
+
+x.grad = None
+y = my_sin(x, threshold=1e-150)
+y.backward()
+print('--- approximate sin ---')
+print(y.data) # 0.7071064695751781 | 0.7071067811865475
+print(x.grad) # 0.7071032148228457 | 0.7071067811865475
 
 x.name = 'x'
 y.name = 'y'
-z.name = 'z'
-plot_dot_graph(z, verbose=False, to_file='goldstein.png')
+plot_dot_graph(y, verbose=False, to_file='my_sin_1e-150.png')
