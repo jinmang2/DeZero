cf20d02862408b559c06f5d98d54a7487203ce07
jinmang2 <jinmang2@gmail.com>
2021-03-24 10:39:40 +0900

    [제 3고지] Step28 함수 최적화 - 로젠브룩 함수(https://en.wikipedia.org/wiki/Rosenbrock_function) - 경사하강법 구현 - 경사하강법은 로젠브록 함수처럼 골짜기가 길게 뻗은 함수엔 잘 대응하지 못함

--- steps/highland3.py
+++ steps/highland3.py
@@ -1,52 +1,43 @@
 if '__file__' in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import math
 import numpy as np
-from dezero import Variable, Function
-from dezero.utils import plot_dot_graph
+from dezero import Variable
+# import dezero's simple_core explicitly
+import dezero
 
 
-class Sin(Function):
-    def forward(self, x):
-        y = np.sin(x)
-        return y
-
-    def backward(self, gy):
-        x = self.inputs[0].data
-        return gy * np.cos(x)
-
-
-def sin(x):
-    return Sin()(x)
-
-
-x = Variable(np.array(np.pi / 4))
-y = sin(x)
-y.backward()
-print('--- original sin ---')
-print(y.data) # 0.7071067811865476
-print(x.grad) # 0.7071067811865476
-
-
-def my_sin(x, threshold=0.0001):
-    y = 0
-    for i in range(100000):
-        c = (-1) ** i / math.factorial(2 * i + 1)
-        t = c * x ** (2 * i + 1)
-        y = y + t
-        if abs(t.data) < threshold:
-            break
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2+ (x0 - 1) ** 2
     return y
 
 
-x.grad = None
-y = my_sin(x, threshold=1e-150)
-y.backward()
-print('--- approximate sin ---')
-print(y.data) # 0.7071064695751781 | 0.7071067811865475
-print(x.grad) # 0.7071032148228457 | 0.7071067811865475
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
 
-x.name = 'x'
-y.name = 'y'
-plot_dot_graph(y, verbose=False, to_file='my_sin_1e-150.png')
+# Calculate Rosenbrock function's derivative
+y = rosenbrock(x0, x1)
+y.backward()
+print(x0.grad, x1.grad) # -2.0 400.0
+
+x0.cleargrad()
+x1.cleargrad()
+
+# Implement Gradient Descent
+lr = 0.001
+iters = 10000
+
+print(x0, x1) # variable(0.0) variable(2.0)
+for i in range(iters):
+
+    y = rosenbrock(x0, x1)
+
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
+print(x0, x1)
+# iters =   1000: variable(0.6837118569138317) variable(0.4659526837427042)
+# iters = 10000: variable(0.9944984367782456) variable(0.9890050527419593)
