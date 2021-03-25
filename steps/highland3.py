5e0c83fa0f5e1946964cbad0286047cd3e769a20
jinmang2 <jinmang2@gmail.com>
2021-03-25 09:54:35 +0900

    [제 3고지] Step29 뉴턴 방법으로 푸는 최적화(수동 계산) - second derivative까지 사용! - 2차 미분을 수동으로 ... 계산하기엔 힘들겠지?

--- steps/highland3.py
+++ steps/highland3.py
@@ -7,5 +7,38 @@ from dezero import Variable
 import dezero
 
 
+def f(x):
+    y = x **4 - 2 * x ** 2
+    return y
+
+
+def gx2(x):
+    """ 2nd derivative! """
+    return 12 * x ** 2 - 4
+
+
 if __name__ == '__main__':
-    pass
+    x = Variable(np.array(2.0))
+    iters = 10
+
+    for i in range(iters):
+        print(i, x)
+
+        y = f(x)
+        x.cleargrad()
+        y.backward()
+
+        x.data -= x.grad / gx2(x.data)
+
+"""
+0 variable(2.0)
+1 variable(1.4545454545454546)
+2 variable(1.1510467893775467)
+3 variable(1.0253259289766978)
+4 variable(1.0009084519430513)
+5 variable(1.0000012353089454)
+6 variable(1.000000000002289)
+7 variable(1.0)
+8 variable(1.0)
+9 variable(1.0)
+"""
