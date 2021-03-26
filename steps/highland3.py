ea86c1f26b5252598fee6f8b9f6c239a0b47d808
jinmang2 <jinmang2@gmail.com>
2021-03-26 19:45:32 +0900

    [제 3고지] Step34 sin 함수 고차 미분 - dezero/functions.py 추가 - sin/cos 고차 미분 구현

--- steps/highland3.py
+++ steps/highland3.py
@@ -4,52 +4,26 @@ if '__file__' in globals():
 import numpy as np
 from dezero import Variable, Function
 # import dezero's simple_core explicitly
+import matplotlib.pyplot as plt
 import dezero
-from dezero.utils import plot_dot_graph
-
-
-def f(x):
-    return x ** 4 - 2 * x ** 2
+import dezero.functions as F
 
 
 if __name__ == '__main__':
-    # (1) Calculate 2nd derivative
-    x = Variable(np.array(2.0))
-    y = f(x)
+    x = Variable(np.linspace(-7, 7, 200))
+    y = F.sin(x)
     y.backward(create_graph=True)
-    print(x.grad) # variable(24.0)
-    gx = x.grad
-    x.cleargrad()
-    gx.backward()
-    print(x.grad) # variable(44.0)
 
-    # (2) Optimization using Newton's method
-    x = Variable(np.array(2.0))
-    iters = 10
+    logs = [y.data]
 
-    for i in range(iters):
-        print(i, x)
-
-        y = f(x)
-        x.cleargrad()
-        y.backward(create_graph=True)
-
+    for i in range(3):
+        logs.append(x.grad.data)
         gx = x.grad
         x.cleargrad()
-        gx.backward()
-        gx2 = x.grad
+        gx.backward(create_graph=True)
 
-        x.data -= gx.data / gx2.data
-
-    """
-    0 variable(2.0)
-    1 variable(1.4545454545454546)
-    2 variable(1.1510467893775467)
-    3 variable(1.0253259289766978)
-    4 variable(1.0009084519430513)
-    5 variable(1.0000012353089454)
-    6 variable(1.000000000002289)
-    7 variable(1.0)
-    8 variable(1.0)
-    9 variable(1.0)
-    """
+    labels = ["y=sin(x)", "y'=cos(x)", "y''=-sin(x)", "y'''=-cos(x)"]
+    for i, v in enumerate(logs):
+        plt.plot(x.data, logs[i], label=labels[i])
+    plt.legend(loc='lower right')
+    plt.show()
