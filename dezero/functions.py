2f7bca8b7e972216bf72f04477add526b3d6a45c
jinmang2 <jinmang2@gmail.com>
2021-03-29 13:12:12 +0900

    [제 3고지] Step35 고차 미분 계산 그래프 - sigmoid(x) = 1 / (1 + exp(-x)) - tanh(x) = 2 * sigmoid(2x) - 1 - tanh(x)/dx = 1 - tanh(x) ** 2 - tanh 함수를 구현하고 이에 대하 7차미분까지 구해보자! - 참 쉽쥬?

--- dezero/functions.py
+++ dezero/functions.py
@@ -34,3 +34,18 @@ class Cos(Function):
 
 def cos(x):
     return Cos()(x)
+
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]() # weakref
+        gx = gy * (1 - y * y)
+        return gx
+
+
+def tanh(x):
+    return Tanh()(x)
--- steps/highland3.py
+++ steps/highland3.py
@@ -7,23 +7,24 @@ from dezero import Variable, Function
 import matplotlib.pyplot as plt
 import dezero
 import dezero.functions as F
+from dezero.utils import plot_dot_graph
 
 
 if __name__ == '__main__':
-    x = Variable(np.linspace(-7, 7, 200))
-    y = F.sin(x)
+    x = Variable(np.array(1.0))
+    y = F.tanh(x)
+    x.name = 'x'
+    y.name = 'y'
     y.backward(create_graph=True)
 
-    logs = [y.data]
+    iters = 7
 
-    for i in range(3):
-        logs.append(x.grad.data)
+    for i in range(iters):
         gx = x.grad
         x.cleargrad()
         gx.backward(create_graph=True)
 
-    labels = ["y=sin(x)", "y'=cos(x)", "y''=-sin(x)", "y'''=-cos(x)"]
-    for i, v in enumerate(logs):
-        plt.plot(x.data, logs[i], label=labels[i])
-    plt.legend(loc='lower right')
-    plt.show()
+    # Figure computational graph
+    gx = x.grad
+    gx.name = 'gx' + str(iters+1)
+    plot_dot_graph(gx, verbose=False, to_file=f'tanh_iters{iters+1}.png')
