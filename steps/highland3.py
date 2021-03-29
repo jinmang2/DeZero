f3c0ab1de08fa3b3051423de205123693deaa001
jinmang2 <jinmang2@gmail.com>
2021-03-29 13:20:06 +0900

    [제 3고지] 고차 미분 이외의 용도 - double backprop! 미분 속의 미분을 풀어야하는 계산에도 이용 - 딥러닝 연구에서의 활용     - WGAN-GP     - MAML(Model-Agnostic Meta-Learning)     - TRPO(True Region Policy Optimization) - 위 구현을 DeZero로 구현할 예정 - 여기서 branch를 뻗어서

--- steps/highland3.py
+++ steps/highland3.py
@@ -11,20 +11,12 @@ from dezero.utils import plot_dot_graph
 
 
 if __name__ == '__main__':
-    x = Variable(np.array(1.0))
-    y = F.tanh(x)
-    x.name = 'x'
-    y.name = 'y'
+    x = Variable(np.array(2.0))
+    y = x ** 2
     y.backward(create_graph=True)
-
-    iters = 7
-
-    for i in range(iters):
-        gx = x.grad
-        x.cleargrad()
-        gx.backward(create_graph=True)
-
-    # Figure computational graph
     gx = x.grad
-    gx.name = 'gx' + str(iters+1)
-    plot_dot_graph(gx, verbose=False, to_file=f'tanh_iters{iters+1}.png')
+    x.cleargrad()
+
+    z = gx ** 3 + y
+    z.backward()
+    print(x.grad)
